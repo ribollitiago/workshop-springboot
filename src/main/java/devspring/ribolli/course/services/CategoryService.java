e603d0ddb3dbf5f6427123778451950034c39ceb
Tiago Ribolli <ribollitiago@gmail.com>
2023-03-27 12:01:55 -0300

    Entity Caregory

---
+++ src/main/java/devspring/ribolli/course/services/CategoryService.java
@@ -0,0 +1,25 @@
+package devspring.ribolli.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import devspring.ribolli.course.entities.Category;
+import devspring.ribolli.course.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+    @Autowired
+    private CategoryRepository repository;
+
+    public List<Category> findAll(){
+        return repository.findAll();
+    }
+
+    public Category findById(Long id){
+        Optional<Category> obj = repository.findById(id);
+        return obj.get();
+    }
+}
